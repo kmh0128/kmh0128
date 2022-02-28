f29f6f240d3eb5e45414cc4c377c250edd56c038
kmh0128 <100178951+kmh0128@users.noreply.github.com>
2022-02-28 16:38:39 +0900

    Add files via upload

---
+++ 코드업 100제/code up 1 java.java
@@ -0,0 +1,10 @@
+package hundered;
+
+public class first {
+
+	public static void main(String[] args) {
+	System.out.println("Hello");
+	}
+
+}
+//코드업 기초 100제 문제:https://codeup.kr/problemsetsol.php
---
+++ 코드업 100제/code up 2 java.java
@@ -0,0 +1,12 @@
+package hundered;
+
+public class seconcd {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
+
+//코드업 기초 100제 문제:https://codeup.kr/problemsetsol.php
