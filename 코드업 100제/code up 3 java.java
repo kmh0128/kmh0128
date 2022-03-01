c298f32a2a5867731f78aeddaa3bf40cb93ee229
kmh0128 <100178951+kmh0128@users.noreply.github.com>
2022-03-02 00:06:19 +0900

    Add files via upload

---
+++ 코드업 100제/code up 3 java.java
@@ -0,0 +1,12 @@
+package hundered;
+
+public class cdu {
+
+	public static void main(String[] args) {
+		System.out.println("Hello\nworld");
+
+	}
+
+}
+
+//참고자료:https://codeup.kr/problemsetsol.php
---
+++ 코드업 100제/code up 4 java.java
@@ -0,0 +1,12 @@
+package hundered;
+
+public class cduf {
+
+	public static void main(String[] args) {
+		System.out.println("'Hello'");
+
+	}
+
+}
+
+//참고자료:https://codeup.kr/problemsetsol.php
