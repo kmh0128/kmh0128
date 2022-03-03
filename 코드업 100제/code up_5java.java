e813cf39cf87744815550fb3be44f373f1cf8863
kmh0128 <100178951+kmh0128@users.noreply.github.com>
2022-03-03 19:04:14 +0900

    Add files via upload

---
+++ 코드업 100제/code up_5java.java
@@ -0,0 +1,9 @@
+package hundered;
+
+public class cdu {
+
+	public static void main(String[] args) {
+		System.out.println("\"Hello World\"");
+	}
+	
+}
---
+++ 코드업 100제/code up_6java.java
@@ -0,0 +1,10 @@
+package hundered;
+
+public class code {
+
+	public static void main(String[] args) {
+		System.out.println("\"!@#$%^&*()\'");
+
+	}
+
+}
