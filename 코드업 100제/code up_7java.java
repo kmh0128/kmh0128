d4503f5a217b5ad186428a4c607ab78ad821ad29
kmh0128 <100178951+kmh0128@users.noreply.github.com>
2022-03-06 21:45:04 +0900

    Add files via upload

---
+++ 코드업 100제/code up_7java.java
@@ -0,0 +1,11 @@
+package hundered;
+
+public class cdu {
+
+	public static void main(String[] args) {
+		System.out.println("\"C:\\Download\\'hello'.py\"");
+
+	}
+	
+}
+//참고자료:https://codeup.kr/problemsetsol.php
---
+++ 코드업 100제/code up_8java.java
@@ -0,0 +1,13 @@
+package hundered;
+
+public class cdue {
+
+	public static void main(String[] args) {
+		System.out.printf("\u250C\u252C\u2510\n");
+	    System.out.printf("\u251C\u253C\u2524\n");
+	    System.out.printf("\u2514\u2534\u2518");
+	}
+
+}
+//참고자료:https://memostack.tistory.com/108
+//코드업 100제:https://codeup.kr/problem.php?id=1008
