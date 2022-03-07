4e4dfa3387790c6701aae038b69d279857319316
kmh0128 <100178951+kmh0128@users.noreply.github.com>
2022-03-07 21:08:35 +0900

    Add files via upload

---
+++ 코드업 100제/code up_10java.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class cdu {
+    public static void main(String[] args) {
+    	int a;
+    	Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        System.out.println("%d"a);
+        
+    }
+}
+
+//참고자료:https://codeup.kr/problem.php?id=1010
