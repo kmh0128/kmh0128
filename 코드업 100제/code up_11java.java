fb85c3368f1871cd6f28a3f8dda430e4a5932d14
kmh0128 <100178951+kmh0128@users.noreply.github.com>
2022-03-11 23:36:41 +0900

    Add files via upload

---
+++ 코드업 100제/code up_11java.java
@@ -0,0 +1,15 @@
+package hundered;
+
+import java.util.*;
+
+public class cdu_11 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		float x = scan.nextFloat();
+		System.out.printf("%f", x);
+	}
+
+}
+
+//코드업 100문제 12번 https://codeup.kr/problem.php?id=1012
