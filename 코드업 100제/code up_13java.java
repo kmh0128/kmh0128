cdc3c3ba61a6fa07d71df5cd6562650974f10650
kmh0128 <100178951+kmh0128@users.noreply.github.com>
2022-03-13 19:47:47 +0900

    Add files via upload

---
+++ 코드업 100제/code up_13java.java
@@ -0,0 +1,17 @@
+package hundered;
+
+import java.util.Scanner;
+public class cd {
+
+	public static void main(String[] args) {
+		Scanner scr = new Scanner(System.in);
+		int a,b;
+		a = scr.nextInt();
+		b = scr.nextInt();
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
+
+//코드업 100제 1013번 https://codeup.kr/problem.php?id=1013
