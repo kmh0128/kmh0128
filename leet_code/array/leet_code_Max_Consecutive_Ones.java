b6506569ea320bf33cab2de6902163c597acaf39
kmh0128 <100178951+kmh0128@users.noreply.github.com>
2022-05-31 23:38:08 +0900

    Add files via upload

---
+++ leet_code/array/leet_code_Max_Consecutive_Ones.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;//연속되는 1을 카운트
+        int max =0;//최대값
+        
+        for(int n : nums) {
+        	if(n==1) {
+        		count ++;//배열을 돌면서 1이 나오면 카운트
+        		if(max < count) =count;//이전에 1을 카운트한값 max가 이후에 카운트한것보다 적으면 max =count값을 해준다
+        	}
+        	else count = 0;//다른경우 1과 0밖에 없습니까 0으로 카운트 해준다
+        }
+        
+        return max;//맥스값 리턴
+    }
+}
+
+//문제자료 밑 참고 자료 :https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3238/
