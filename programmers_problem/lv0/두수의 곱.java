2404c022754329d9f495159de1c63e9b463e7536
kmh0128 <100178951+kmh0128@users.noreply.github.com>
2022-10-03 23:26:31 +0900

    Create 두수의 곱.java

---
+++ programmers_problem/lv0/두수의 곱.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = num1 * num2;
+        return answer;
+    }
+}
+
+//문제 주소 ->https://school.programmers.co.kr/learn/courses/30/lessons/120804?language=java
