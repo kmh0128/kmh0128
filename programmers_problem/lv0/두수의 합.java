d3b9bf348e2c720f5b95fc8c068f45230b22260c
kmh0128 <100178951+kmh0128@users.noreply.github.com>
2022-10-05 22:28:42 +0900

    Create 두수의 합.java

---
+++ programmers_problem/lv0/두수의 합.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = num1 + num2;
+        return answer;
+    }
+}
+
+https://school.programmers.co.kr/learn/courses/30/lessons/120802
