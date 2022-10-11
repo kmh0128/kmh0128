d339e55457288a6d3fd0fd6e84a4e6748bd88142
kmh0128 <100178951+kmh0128@users.noreply.github.com>
2022-10-11 22:08:16 +0900

    Create 나머지 구하기.java

---
+++ programmers_problem/lv0/나머지 구하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = -1;
+        return num1%num2;
+    }
+}
+
+문제 출처 https://school.programmers.co.kr/learn/courses/30/lessons/120810
