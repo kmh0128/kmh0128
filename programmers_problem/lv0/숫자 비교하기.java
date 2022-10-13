7cabace2e6f94087a27f639129dc84d39a67e345
kmh0128 <100178951+kmh0128@users.noreply.github.com>
2022-10-13 21:48:29 +0900

    Create 숫자 비교하기.java

---
+++ programmers_problem/lv0/숫자 비교하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        return num1 == num2 ? 1 : -1;//삼항 연산자를 이용해서 나누어 떨어질때는 1 나누어 떨어지지 않을때는 -1로 출력
+    }
+}
+
+//문제: https://school.programmers.co.kr/learn/courses/30/lessons/120807
