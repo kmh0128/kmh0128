94dce5e866aca529c38ba08b202362a24d17ed04
kmh0128 <100178951+kmh0128@users.noreply.github.com>
2022-10-15 21:39:16 +0900

    Create 두수의 차.java

---
+++ programmers_problem/lv0/두수의 차.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        return (num1 - num2);
+    }
+}
+
+
+문제: https://school.programmers.co.kr/learn/courses/30/lessons/120803
