5b8b762398918fc82d699365bc591dbaa342bedb
kmh0128 <100178951+kmh0128@users.noreply.github.com>
2022-10-15 21:37:50 +0900

    Create 나이 구하기.java

---
+++ programmers_problem/lv0/나이 구하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int age) {
+        int answer = 0;
+        return 2022 - age + 1;
+    }
+}
+
+
+문제 출처:https://school.programmers.co.kr/learn/courses/30/lessons/120820
