efc9de22ce541078c28e39be428092d62619c758
kmh0128 <100178951+kmh0128@users.noreply.github.com>
2023-03-24 20:35:49 +0900

    Create 배열 원소의 길이.java

---
+++ programmers_problem/lv0/배열 원소의 길이.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(String[] strlist) {
+        int[] answer = new int[strlist.length];//int타입의 배열을 초기화 그리고 strlist의 길이만큼 변수의 크기를 지정해준다
+
+        for (int i = 0; i < answer.length; i++) {//배열의 길이만큼 반복
+            answer[i] = strlist[i].length();//strlist[i].length()를  strlist의 i의 위치에있는 값의 길이를 answer에 대입합니다.
+        }
+        return answer;
+    }
+}
+
+문제: https://school.programmers.co.kr/learn/courses/30/lessons/120854
