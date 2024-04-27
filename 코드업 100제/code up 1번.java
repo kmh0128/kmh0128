408d4ec05cced957f709284fa6ff74671655e99b
kmh0128 <100178951+kmh0128@users.noreply.github.com>
2024-04-27 22:37:55 +0900

    Update code up 1번.java

--- 코드업 100제/code up 1번.java
+++ 코드업 100제/code up 1번.java
@@ -9,3 +9,8 @@ public class first {
 }
 //코드업 기초 100제 문제:https://codeup.kr/problemsetsol.php
 //지정한 문장을 출력하는문제
+/*
+public static void main(String[] args) 에 대하여 공부하고 기록한것
+
+https://github.com/kmh0128/kmh0128/blob/main/%EC%9D%B4%EA%B2%83%EC%A0%80%EA%B2%83/%EC%9E%90%EB%B0%94%EC%9D%98%20%EB%A9%94%EC%9D%B8%20%EB%A9%94%EC%84%9C%EB%93%9C%EB%8A%94%20%EC%99%9C%20public%20static.md
+*/
