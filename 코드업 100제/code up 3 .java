543aa5b995cabb09d4f2db8ce1f335df032ed686
kmh0128 <100178951+kmh0128@users.noreply.github.com>
2024-05-18 22:55:26 +0900

    Update and rename code up 3 java.java to code up 3 .java

---
+++ 코드업 100제/code up 3 .java
@@ -0,0 +1,28 @@
+package hundered;
+
+public class cdu {
+
+	public static void main(String[] args) {
+		System.out.println("Hello\nworld");
+
+	}
+
+}
+
+/*
+문제 출처  https://codeup.kr/problemsetsol.php
+*/
+
+//다른방식
+
+/*
+
+public class problem2 {
+    public static void main(String[] args){
+        System.out.println( "Hello" + System.lineSeparator() +"World");
+    }
+}
+
+ System.lineSeparator() 사용
+
+*/
