c7705de70e52e6841a4d98a1b244c23ffcfcd042
kmh0128 <100178951+kmh0128@users.noreply.github.com>
2024-05-27 23:54:50 +0900

    Update code up 4 .java

--- 코드업 100제/code up 4 .java
+++ 코드업 100제/code up 4 .java
@@ -10,3 +10,14 @@ public class cduf {
 }
 
 //문제 주소 :https://codeup.kr/problemsetsol.php
+
+/*
+결과
+
+'Hello'
+
+*/
+
+//다른방식 System.out.println("\'Hello\'") ->로도 작은따옴표가 출력된다.
+
+//이스케이프 시퀸스 참고자료: https://grandma-coding.tistory.com/entry/Java-%EB%AC%B8%EC%9E%90%EC%97%B4String%EC%97%90-%ED%81%B0%EB%94%B0%EC%98%B4%ED%91%9C-%EC%9E%91%EC%9D%80-%EB%94%B0%EC%98%B4%ED%91%9C-%EB%84%A3%EB%8A%94-%EB%B0%A9%EB%B2%95
