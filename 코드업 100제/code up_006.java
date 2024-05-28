f06bdf8fc7a20ee81179d5ef9f05dc6969e780d9
kmh0128 <100178951+kmh0128@users.noreply.github.com>
2024-05-28 23:17:51 +0900

    Update and rename code up_6java.java to code up_006.java

---
+++ 코드업 100제/code up_006.java
@@ -0,0 +1,13 @@
+package hundered;
+
+public class code {
+
+	public static void main(String[] args) {
+		System.out.println("\"!@#$%^&*()\"");
+
+	}
+
+}
+
+//참고자료:https://codeup.kr/problemsetsol.php
+//큰따옴펴 추력 문제
