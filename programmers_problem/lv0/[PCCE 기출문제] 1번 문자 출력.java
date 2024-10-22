02d790c0e5ebbb49b0647f6892bc82dc713baf28
kmh0128 <100178951+kmh0128@users.noreply.github.com>
2024-10-22 23:39:41 +0900

    Create [PCCE 기출문제] 1번 문자 출력.java

---
+++ programmers_problem/lv0/[PCCE 기출문제] 1번 문자 출력.java
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    string message = "Let's go!";
+
+    cout << "3\n2\n1" << endl;//3다음 2다음 1 이 출력되게
+    cout << message << endl;//숫자가 출력된다음 메시지가 출력되게한다.
+
+    return 0;
+}
+
+
+//문제출처 -> https://school.programmers.co.kr/learn/courses/30/lessons/340207
