64215fca4bc82d0918a87258e8b74a8d7e388946
kmh0128 <100178951+kmh0128@users.noreply.github.com>
2024-12-26 22:08:15 +0900

    Update 특수문자 출력하기.java

--- programmers_problem/특수문자 출력하기.java
+++ programmers_problem/특수문자 출력하기.java
@@ -1,8 +1,9 @@
-#include <stdio.h>
+import java.util.Scanner;
 
-int main(void) {
-    return 0;
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
+    }
 }
-
 
 //출처 프로그래머스 사이트 https://school.programmers.co.kr/learn/challenges?order=recent&levels=0&languages=java
