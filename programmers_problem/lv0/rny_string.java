f530981de397f20847fb80cc39e0c571f2c0b6b2
kmh0128 <100178951+kmh0128@users.noreply.github.com>
2024-12-31 22:51:58 +0900

    Create rny_string.java

---
+++ programmers_problem/lv0/rny_string.java
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* rny_string) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    char* answer = (char*)malloc(1);
+    return answer;
+}
+
+
+//문제출처: 프로그래머스
+//https://school.programmers.co.kr/learn/courses/30/lessons/181863
