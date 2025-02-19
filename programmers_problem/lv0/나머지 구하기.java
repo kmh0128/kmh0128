98e95518363a87d1d52a8239fc549569bd690d0f
kmh0128 <100178951+kmh0128@users.noreply.github.com>
2025-02-19 21:54:12 +0900

    Update 나머지 구하기.java

--- programmers_problem/lv0/나머지 구하기.java
+++ programmers_problem/lv0/나머지 구하기.java
@@ -1,8 +1,28 @@
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = -1;
         return num1%num2;
     }
 }
 
-문제 출처 https://school.programmers.co.kr/learn/courses/30/lessons/120810
+//문제 출처 https://school.programmers.co.kr/learn/courses/30/lessons/120810
+
+
+/*
+문제 설명
+
+정수 num1, num2가 매개변수로 주어질 때, num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
+*/
+
+/*
+제한사항
+0 < num1 ≤ 100
+0 < num2 ≤ 100
+*/
+
+/*
+입출력 예
+
+num1	num2	result
+3	    2	     1
+10	    5	     0
+*/
